e6b04ee15f176a54129fded2862c581960536e7c
Daniel Mbata <dnm@macbookpro.home>
2024-10-19 11:45:12 +0100

    moving to github

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from "./App";
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+// Create a root element
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
